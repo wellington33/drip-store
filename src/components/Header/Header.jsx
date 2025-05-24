14ca0e4b934b7fa70a6af86b6706df7caa42cf28
wellington33 <wellingtonm938@gmail.com>
2025-05-24 00:48:08 -0300

    criando projeto drip-store

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Drip Store</h1>
+        </header>
+    );
+
+}
+
+export default Header;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/Header";
+
+function HomePage() {
+    return (
+        <div>
+            <Header />
+            <p>Bem-Vindo à Drip Store</p>
+        </div>
+    );
+}
+
+export default HomePage;
